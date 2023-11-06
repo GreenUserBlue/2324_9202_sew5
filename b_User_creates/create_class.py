b13be567d7a207f3142d6b93c268bab8cc023696
Felix Zwickelstorfer <felix.zwickelstorfer@htl.rennweg.at>
2023-11-06 15:41:23 +0100

    delete also works

--- b_User_creates/create_class.py
+++ b_User_creates/create_class.py
@@ -7,6 +7,7 @@ from openpyxl import load_workbook
 
 __author__ = "Zwickelstorfer Felix"
 
+
 # def start_program():
 #     """runs the logic for the program"""
 #     args = sys.argv
@@ -24,68 +25,82 @@ __author__ = "Zwickelstorfer Felix"
 
 
 def getSafeFilePaths(outputDir):
+    return f"{outputDir}/creates.sh", f"{outputDir}/deletes.sh", f"{outputDir}/list.csv", f"{outputDir}/logfile.log"
 
-    return f"{outputDir}/creates.sh",f"{outputDir}/deletes.sh",f"{outputDir}/list.csv",f"{outputDir}/logfile.log"
+
+def getUserNameSpecialChars(username):
+    uName = str(username).lower()
+    if uName[0].isnumeric():
+        uName = f"k{uName}"
+    return uName
 
 
-def addCreateCommand(createFile, x):
-    createFile.write(f'useradd -d "/home/klassen/k{str(x[0]).lower()}" -c "{x[0]} - {x[2]}" -m -g "klasse" -G cdrom,plugdev,sambashare -s /bin/bash k{str(x[0]).lower()}\n')
-    createFile.write(f'echo "k{str(x[0]).lower()}:{str(x[0]).lower()}{get_password_random_char()}{x[1]}{get_password_random_char()}{x[2].lower()}{get_password_random_char()}" | chpasswd\n')
-    pass
+def addCreateCommand(createFile, x, addedUsers):
+    uName = getUserNameSpecialChars(x[0])
+    if uName in addedUsers:
+        raise Exception(f"User ${uName} already exists!")
+    addedUsers.append(uName)
+    createFile.write(
+        f'useradd -d "/home/klassen/{uName}" -c "{x[0]} - {x[2]}" -m -g "klasse" -G cdrom,plugdev,sambashare -s /bin/bash {uName}\n')
+    createFile.write(
+        f'echo "{uName}:{str(x[0]).lower()}{get_password_random_char()}{x[1]}{get_password_random_char()}{x[2].lower()}{get_password_random_char()}" | chpasswd\n')
 
 
 def get_password_random_char():
     passwdRndChar = "!%&(),._-=^#5"
     return passwdRndChar[math.floor(random.random() * len(passwdRndChar))]
+
+
 def addDeleteCommand(deleteFile, x):
-    # deleteFile.write(x)
+    uName = getUserNameSpecialChars(x[0])
+    deleteFile.write("userdel -r "+str(uName)+"\n")
     pass
+
+
+def addCommands(createFile, deleteFile, x, addedUsers):
+    addCreateCommand(createFile, x, addedUsers)
+    addDeleteCommand(deleteFile, x)
 
 
 def createClasses(path, outputDir):
     os.makedirs(outputDir, exist_ok=True)
     wb = load_workbook(path, read_only=True)
     ws = wb[wb.sheetnames[0]]
-    createPath,deletePath,listingPath,logPath = getSafeFilePaths(outputDir)
-    with open(createPath,"w") as createFile, open(deletePath,"w") as deleteFile:
+    createPath, deletePath, listingPath, logPath = getSafeFilePaths(outputDir)
+    with open(createPath, "w") as createFile, open(deletePath, "w") as deleteFile:
+        createFile.write("#! /bin/sh\ngroupadd klasse\n")
+        deleteFile.write("#! /bin/sh\n")
+
+        addedUsers = []
         for row in ws.iter_rows(min_row=2):
-            x = row[0].value,row[1].value,row[2].value
-            print(x)
-            addCreateCommand(createFile, x)
-            addDeleteCommand(deleteFile,x)
+            x = row[0].value, row[1].value, row[2].value
+            if x[0] is None:
+                break
+            addCommands(createFile, deleteFile, x, addedUsers)
+
+        addCommands(createFile, deleteFile, ("lehrer", "0", "JUE"), addedUsers)  # Jüngling
+        addCommands(createFile, deleteFile, ("seminar", "0", "JUE"), addedUsers)  # Jüngling
 
 
-#  ein Bash-Script2 mit allen notwendigen Schritten/Befehlen zum Erzeugen der Benutzer3
+# - ein Bash-Script2 mit allen notwendigen Schritten/Befehlen zum Erzeugen der Benutzer3
 # – ein Bash-Script zum Löschen der angelegten Benutzer
 # – eine Liste mit Usernamen und Passwörtern zum Verteilen an die unterrichtenden Lehrer
 # – ein Logfile mit sinnvollen Angaben
-# • dabei ist zu beachten
+
+
 # – der Dateiname für die Eingabedatei soll auf der Kommandozeile angegeben werden (ohne vorangestelltes
 # -f o.ä.).
 # – mit den Optionen -v für verbose und -q für quite soll der Umfang des Loggings eingestellt werden können
 # (Loglevel).
 # ∗ damit sollten sich auch die Bash-Scripts ändern
-# – die Benutzernamen sollen
-# ∗ Klassen erhalten ein zusätzliches k als erstes Zeichen – der Name sollte nicht mit einer Ziffer beginnen
-# ∗ sollen in Kleinbuchstaben angegeben werden
-# ∗ etwaige Sonderzeichen (Umlaute) sollten ersetzt werden
-# ∗ der Kommentar (gecos field) sollte auf einen sinnvollen Wert gesetzt werden
-# – es gibt zwei zusätzliche Benutzer: lehrer und seminar
 
 # Die Home-Verzeichnisse der Benutzer soll unter /home/klassen bzw. /home/lehrer/ angelegt werden,
-# z.B. /home/klassen/k1ai
-# ∗ diese Ordner existieren eventuell nicht
-# – Die Linux-User müssen in folgenden existierenden System-Gruppen sein: cdrom,plugdev,sambashare
 
 # – Wichtig: eventuell braucht man im Script ein Escape (“\”) beim Passwort, aber nicht im Textfile!
-# – Man sollte auch
-# ∗ den Benutzername (“Gecos”)
-# ∗ eine sinnvolle Shell (/bin/bash)
-# ∗ das Passwort (mittels chpasswd)
-# setzen
-# .
+
 # – bei bereits existierenden Benutzern sollte eine Fehlermeldung erfolgen.
 # – die Bash-Scripts sollten bei Problemen abbrechen
+
 # – Logging
 # ∗ Wir wollen einen RotatingFileHandler mit maximal 10.000 Bytes7 und maximal 5 alte Versionen
 # (backup).
