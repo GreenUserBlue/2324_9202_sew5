7a75e704538c4d218f375c9c677052684755fc66
Felix Zwickelstorfer <felix.zwickelstorfer@htl.rennweg.at>
2023-11-27 17:07:35 +0100

    added the search methods

--- java/src/c_labyrinth/Labyrinth.java
+++ java/src/c_labyrinth/Labyrinth.java
@@ -77,13 +77,13 @@ public class Labyrinth {
      * @param lab
      */
     public static void printLabyrinth(char[][] lab) {
-		for (int i = 0; i < lab.length; i++) {
-			System.out.println();
-			for (int j = 0; j < lab[i].length; j++) {
-				System.out.print(lab[i][j]);
-			}
-		}
-		System.out.println();
+        for (char[] chars : lab) {
+            System.out.println();
+            for (char aChar : chars) {
+                System.out.print(aChar);
+            }
+        }
+        System.out.println();
     }
 
     /**
@@ -95,15 +95,53 @@ public class Labyrinth {
      * @throws InterruptedException für die verlangsamte Ausgabe mit sleep()
      */
     public static boolean suchen(int zeile, int spalte, char[][] lab) throws InterruptedException {
-        // TODO Code fehlt noch
-        // nur lab[zeile][spalte] betrachten
+        if (zeile < 0 && spalte < 0 || zeile >= lab.length || spalte >= lab[0].length) return false;
+        if (lab[zeile][spalte] == 'A') return true;
+        if (lab[zeile][spalte] == '#' || lab[zeile][spalte] == 'X') return false;
+        char old = lab[zeile][spalte];
+        lab[zeile][spalte] = 'X';
+
+        if (suchen(zeile + 1, spalte, lab)) return true;
+        if (suchen(zeile, spalte + 1, lab)) return true;
+        if (suchen(zeile - 1, spalte, lab)) return true;
+        if (suchen(zeile, spalte - 1, lab)) return true;
+
+        lab[zeile][spalte] = old;
         return false;
     }
 
+
+    /**
+     * Suche alle möglichen Wege
+     *
+     * @param zeile  aktuelle Position
+     * @param spalte aktuelle Position
+     * @param lab
+     * @throws InterruptedException für die verlangsamte Ausgabe mit sleep()
+     */
+    public static int suchenAlle(int zeile, int spalte, char[][] lab) throws InterruptedException {
+        if (zeile < 0 && spalte < 0 || zeile >= lab.length || spalte >= lab[0].length) return 0;
+        if (lab[zeile][spalte] == 'A') return 1;
+        if (lab[zeile][spalte] == '#' || lab[zeile][spalte] == 'X') return 0;
+        char old = lab[zeile][spalte];
+        lab[zeile][spalte] = 'X';
+        int amount = 0;
+        amount += suchenAlle(zeile + 1, spalte, lab);
+        amount += suchenAlle(zeile, spalte + 1, lab);
+        amount += suchenAlle(zeile - 1, spalte, lab);
+        amount += suchenAlle(zeile, spalte - 1, lab);
+        lab[zeile][spalte] = old;
+        return amount;
+    }
+
     public static void main(String[] args) throws InterruptedException {
-        char[][] labyrinth = fromStrings(maps[0]);
+
+        var map=2;
+        char[][] labyrinth = fromStrings(maps[map]);
         printLabyrinth(labyrinth);
         System.out.println("Ausgang gefunden: " + (suchen(5, 5, labyrinth) ? "ja" : "nein"));
-        // TODO: System.out.println("Anzahl Wege: " + suchenAlle(5, 5, labyrinth));
+
+        labyrinth = fromStrings(maps[map]);
+        System.out.println("Anzahl Wege: " + suchenAlle(5, 5, labyrinth));
     }
 }
