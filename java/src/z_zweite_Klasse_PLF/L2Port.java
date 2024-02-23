b701eae5ecc331104b5b1bd0989660ed2768e962
Felix Zwickelstorfer <felix.zwickelstorfer@htl.rennweg.at>
2024-02-23 18:43:14 +0100

    added extra vars

--- java/src/z_zweite_Klasse_PLF/L2Port.java
+++ java/src/z_zweite_Klasse_PLF/L2Port.java
@@ -1,8 +1,12 @@
 package z_zweite_Klasse_PLF;
 
+import java.util.Objects;
+
 public class L2Port {
 
     protected long mac;
+
+    protected L2Port connectedTo;
 
     public long getMaxSpeed() {
         return maxSpeed;
@@ -23,5 +27,23 @@ public class L2Port {
         String s = String.format("%012X", mac);
         s = s.replaceAll("(..)", "$1-");
         return "MAC address: " + s.substring(0, s.length() - 1) + ", maximum speed: " + maxSpeed + " bps";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        L2Port l2Port = (L2Port) o;
+        return mac == l2Port.mac;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(mac);
+    }
+
+    public void connectTo(L2Port port) {
+        port.connectedTo = this;
+        connectedTo = port;
     }
 }
