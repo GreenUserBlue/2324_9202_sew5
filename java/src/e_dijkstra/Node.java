abf6aa76cabf537b2ba1c105834e5421ac8e416e
Felix Zwickelstorfer <felix.zwickelstorfer@htl.rennweg.at>
2024-03-08 14:13:30 +0100

    added javadoc to the node

--- java/src/e_dijkstra/Node.java
+++ java/src/e_dijkstra/Node.java
@@ -5,24 +5,45 @@ import java.util.List;
 import java.util.PriorityQueue;
 import java.util.stream.Collectors;
 
+
+/**
+ * @author Zwickelstorfer Felix
+ *
+ * represents a mathematical node
+ */
 class Node implements Comparable<Node> {
 
     public String getId() {
         return id;
     }
 
+    /**
+     * the unique identifier
+     */
     private final String id;
 
+    /**
+     * all edges the nodes has to other elements
+     */
     private final List<Edge> edges = new ArrayList<>();
 
+    /**
+     * the current distance the node has to the start node
+     */
     private int distance = Integer.MAX_VALUE;
 
+    /**
+     * the previous node for the shortet route
+     */
     private Node previous = null;
 
     public boolean isVisited() {
         return isVisited;
     }
 
+    /**
+     * if this node was visited before
+     */
     private boolean isVisited = false;
 
     public Node(String id) {
@@ -64,6 +85,11 @@ class Node implements Comparable<Node> {
         distance = 0;
     }
 
+
+    /**
+     * handles what should happen when the node is visited
+     * @param queue the queue with the next ndoes to visit
+     */
     public void visit(PriorityQueue<Node> queue) {
         isVisited = true;
         for (Edge edge : edges) {
@@ -77,10 +103,16 @@ class Node implements Comparable<Node> {
         }
     }
 
+    /**
+     * @return the edge to string beautifully
+     */
     public String edgesToStr() {
         return edges.stream().map(it -> it.getNeighbour().id + ":" + it.getDistance()).collect(Collectors.joining(" ", "", ""));
     }
 
+    /**
+     * if it is the start node
+     */
     public boolean isFirst() {
         //man braucht beides, da die distance zu einem neighbor 0 sein kann, oder wenn etwas nicht erreichbar das previous auch null ist
         return previous == null && distance == 0;
