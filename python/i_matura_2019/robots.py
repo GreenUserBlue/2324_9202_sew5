103021025493a1fe3d4095419f41108e97cfa179
felix <9202@htl.rennweg.at>
2024-04-23 18:27:50 +0200

    added typehints

--- python/i_matura_2019/robots.py
+++ python/i_matura_2019/robots.py
@@ -1,16 +1,14 @@
 import argparse
 import logging
+from typing import Tuple, Set, List
 
 
-def search():
-    pass
-
-
-def get_surrounding_pos(pos: tuple[int, int]):
+def get_surrounding_pos(pos: Tuple[int, int]) -> List[Tuple[int, int]]:
     return [(pos[0] + 1, pos[1]), (pos[0] - 1, pos[1]), (pos[0], pos[1] + 1), (pos[0], pos[1] - 1)]
 
 
-def get_data(data: list[str]):
+def get_data(data: List[str]) -> Tuple[
+    Set[Tuple[int, int]], Set[Tuple[int, int]], Set[Tuple[int, int]], Tuple[int, int]]:
     robots, rechargers, emtpy_spaces, targets = set(), set(), set(), set()
 
     for i in range(len(data)):
@@ -36,7 +34,7 @@ def get_data(data: list[str]):
 # ------------------------------------------ The follwing solution is deapth-first search, which is, even though it is optimised, too slow for room_3.txt
 # ------------------------------------------
 
-# def solve_single_robot(r, open_targets, emtpy_spaces, path: list = None, best_path_len=-1):
+# def solve_single_robot(r, open_targets, emtpy_spaces, path: List = None, best_path_len=-1):
 #     if path is None:
 #         path = []
 #     if r in open_targets:
@@ -65,7 +63,8 @@ def get_data(data: list[str]):
 #         return []
 #     return path
 
-def solve_single_robot(r, open_targets, emtpy_spaces):
+def solve_single_robot(r: Tuple[int, int], open_targets: List[Tuple[int, int]], emtpy_spaces: List[Tuple[int, int]]) -> \
+        List[Tuple[int, int]]:
     logger = logging.getLogger("wegsuche")
     if r in open_targets:
         logger.debug("found solution after 0 states, len: 1")
@@ -84,7 +83,7 @@ def solve_single_robot(r, open_targets, emtpy_spaces):
             if len(direct_targets) > 0:
                 path.append(direct_targets[0])
                 logger.debug("found solution after " + str(counter) + " states, len: " + str(len(path)))
-                logger.info("solution: "+str(r)+" -> "+str(path))
+                logger.info("solution: " + str(r) + " -> " + str(path))
                 return path
             for i in next_gen:
                 new_path = path.copy()
@@ -96,7 +95,7 @@ def solve_single_robot(r, open_targets, emtpy_spaces):
     return []
 
 
-def compute_file(path: str):
+def compute_file(path: str) -> None:
     logger = logging.getLogger("wegsuche")
     logger.info("Reading " + str(path))
     with open(path) as f:
@@ -122,7 +121,7 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
-def setup_logger(args):
+def setup_logger(args: argparse.Namespace) -> None:
     log_level = logging.INFO
     if args.verbose:
         log_level = logging.DEBUG
