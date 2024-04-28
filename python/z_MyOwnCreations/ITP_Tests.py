102c829082ea2a49e3133623a5d0d5afe2c0369c
felix <9202@htl.rennweg.at>
2024-04-28 19:45:51 +0200

    fixed wrong part with sierpinski

--- python/z_MyOwnCreations/ITP_Tests.py
+++ python/z_MyOwnCreations/ITP_Tests.py
@@ -1,9 +1,25 @@
-def meth(a: list[str]):
-    a.append("asdfasdf")
+# def meth(a: list[str]):
+#     a.append("asdfasdf")
+#
+#
+# b=["asdf"]
+#
+# meth(b)
+#
+# print(b)
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(10, 8))
+fig, ax = plt.gcf(), plt.gca()
+points, = ax.plot(range(10), 'ro')
 
 
-b=["asdf"]
 
-meth(b)
+print(data_to_pixel_coords_within_plot((0, 0), ax))
+print(pixel_to_data_coords_within_plot(data_to_pixel_coords_within_plot((5,5),ax),ax))
 
-print(b)
+# We have to be sure to save the figure with it's current DPI
+# (savfig overrides the DPI of the figure, by default)
+# fig.savefig('test.png', dpi=fig.dpi)
+plt.show()
